c826ea17b70f26fc0d70c00f2f9a352ba5c10a6b
andrescalderonart <andresfelipec840@gmail.com>
2025-05-23 19:18:36 -0500

    Se agrego la dependecia validation

--- src/main/java/com/example/domain/Venta.java
+++ src/main/java/com/example/domain/Venta.java
@@ -1,4 +1,25 @@
 package com.example.domain;
 
-public class Venta {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table (name = "venta")
+public class Venta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_venta;
+
+    private String fecha;
+    private Double total;
+
+    @ManyToOne
+    @JoinColumn(name = "id_vendedor", referencedColumnName = "id_vendedor")
+    private Vendedor vendedor;
 }
