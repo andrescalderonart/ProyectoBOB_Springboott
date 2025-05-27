5e195a50ac1ee90f91451ba56a209ac520823bda
andrescalderonart <andresfelipec840@gmail.com>
2025-05-27 18:46:39 -0500

    Se agrego el pomp Security

--- src/main/java/com/example/web/ConfSeg.java
+++ src/main/java/com/example/web/ConfSeg.java
@@ -2,7 +2,10 @@ package com.example.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 
@@ -10,6 +13,17 @@ public class ConfSeg {
 
     @Bean
     public UserDetailsService users(){
+        UserDetails user = User.withUsername("user")
+                .password("{noop}210607")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}123456")
+                .roles("ADMIN")
+                .build();
+        return InMemoryUserDetailsManager (user,admin);
 
     }
+
 }
