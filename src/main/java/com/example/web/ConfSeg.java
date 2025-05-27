515cea536143bd856c2b669c7820b91c66d2763a
andrescalderonart <andresfelipec840@gmail.com>
2025-05-27 18:44:36 -0500

    Se agrego el pomp Security

---
+++ src/main/java/com/example/web/ConfSeg.java
@@ -0,0 +1,15 @@
+package com.example.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+
+public class ConfSeg {
+
+    @Bean
+    public UserDetailsService users(){
+
+    }
+}
