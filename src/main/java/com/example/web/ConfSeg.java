e453eddce0bf4449a5cc071ffc5572c4faf17747
andrescalderonart <andresfelipec840@gmail.com>
2025-05-27 19:14:55 -0500

    Se agrego un nuevo archivo en la carpeta de web

--- src/main/java/com/example/web/ConfSeg.java
+++ src/main/java/com/example/web/ConfSeg.java
@@ -2,10 +2,12 @@ package com.example.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 
@@ -25,5 +27,16 @@ public class ConfSeg {
         return new InMemoryUserDetailsManager (user,admin);
 
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.anyRequest().authenticated()
+                )
+                .formLogin(httpSecurityFormLoginConfigurer ->
+                        httpSecurityFormLoginConfigurer.permitAll())
+                .httpBasic(httpSecurityHttpBasicConfigurer -> {});
 
+        return http.build();
+    }
 }
