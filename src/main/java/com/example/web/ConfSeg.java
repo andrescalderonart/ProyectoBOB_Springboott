ce43dee6e35f24d08ee2e8f431e7238284649bc4
andrescalderonart <andresfelipec840@gmail.com>
2025-07-04 18:59:42 -0500

    Se empezo a configurar el control de perfiles y tambien se inicio a configurar el html del login

--- src/main/java/com/example/web/ConfSeg.java
+++ src/main/java/com/example/web/ConfSeg.java
@@ -1,14 +1,21 @@
 package com.example.web;
 
 import com.example.servicio.UsuarioDetailsServices;
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -42,10 +49,9 @@ public class ConfSeg {
         http
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
-                                .requestMatchers("/css/**","/js/**","/login").permitAll()
-                                .requestMatchers("/indice/**").hasRole("Administrador")
-                                .requestMatchers("/secretaria/**").hasRole("USER, Administrador")
-                                .anyRequest().authenticated()
+                                .requestMatchers("/css/","/js/","/login").permitAll()
+                                .anyRequest()
+                                .authenticated()
                 )
                 .formLogin( form -> form
                         .loginPage("/login")
